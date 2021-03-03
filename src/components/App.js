e59f630583546c640fbc722d13903df4425f2e9e
Rukundo Eric <ericrukundo005@gmail.com>
2021-03-03 15:41:05 +0200

    create components files

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,33 @@
-import logo from '../assets/images/logo.svg';
+import { Component } from 'react';
 import '../assets/css/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          calculator app
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: 0,
+    };
+  }
+
+  render() {
+    const { result } = this.state;
+    return (
+      <div className="App">
+        <div className="calc-container">
+          <div className="calc-content">
+            <table border="1">
+              <tr>
+                <td><div>{result}</div></td>
+              </tr>
+              <tr>
+                <td></td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
